ef929ca0559fd6b7d59c12e5e84c0475a4241051
Suman Maity <suman.maity412@gmail.com>
2020-10-28 10:18:57 +0530

    initial commit for master

---
+++ src/main/java/com/suman/basicAppWithRolePriviledge/BasicAppWithRolePriviledgeApplication.java
@@ -0,0 +1,13 @@
+package com.suman.basicAppWithRolePriviledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAppWithRolePriviledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAppWithRolePriviledgeApplication.class, args);
+	}
+
+}
